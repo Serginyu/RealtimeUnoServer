811676d968b65ef6156b27921dd398385f7d52c0
Sergio Novo <sergionovo1997@gmail.com>
2020-12-05 19:32:40 +0100

    test action coverage

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     '^.+\\.ts?$': 'ts-jest'
   },
   testEnvironment: 'node',
+  coverageReporters: ["text-summary", "html"],
   testRegex: './src/.*\\.(test|spec)?\\.(ts|ts)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   "roots": [
