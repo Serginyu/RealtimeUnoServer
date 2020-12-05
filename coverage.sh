b964ec3840d30fe3accb4f20d4dd57c054443fe5
Sergio Novo <sergionovo1997@gmail.com>
2020-12-05 19:56:43 +0100

    test action coverage

--- coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This scripts counts the lines of code and comments in all source files
-# and prints the results to the command line. It uses the commandline tool
-# "cloc". You can either pass --loc, --comments or --percentage to show the
-# respective values only.
-# Some parts below need to be adapted to your project!
-
-jest --coverage | awk -F "Lines        : " '{print $2}'
